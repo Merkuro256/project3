559e752b2c87be6269bfe4d0ab14aef77e66d310
bakasempai66 <dysenka03@bk.ru>
2024-05-29 20:00:33 +0300

    users: add login & register pages

--- teach/settings.py
+++ teach/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'elements',
+    'users',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,7 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = '/elements'
